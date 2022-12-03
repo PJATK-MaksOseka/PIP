987ab1035d06d19a11883a7fbc364f4547303991
PJATK-MaksOseka <117570694+PJATK-MaksOseka@users.noreply.github.com>
2022-12-03 22:04:16 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-
-        System.out.println("Witamy w kantorze!" + "\n" + "Wybierz walutę:" + "\n" + " 1 - PLN" + "\n" + " 2 - JPY");
-
-
-
-    }
-}
